2de9008d1c96de03d1cdd15383bf3438355c4297
nthoang-apcs <nthoang@apcs.vn>
2017-02-13 22:48:35 +0700

    Initial Work

--- Source-Codes/IERSystem/IERSystem/Areas/Administrator/Models/MauPTInputModel.cs
+++ Source-Codes/IERSystem/IERSystem/Areas/Administrator/Models/MauPTInputModel.cs
@@ -1,27 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace IERSystem.Areas.Administrator.Models
 {
-    public class MauLayHienTruong
+    [Serializable]
+    public class MauPTInputModel
     {
-        public int Id { get; set; }
         public String MaMau { get; set; }
         public String ViTriLayMau { get; set; }
         public uint Soluong { get; set; }
-        public String DonVi { get; set; }
+        //public String DonVi { get; set; }
         public String MoTaMau { get; set; }
         //public DateTime NgayNhanMau { get; set; }
         //public DateTime NgayTraMau { get; set; }
-        public int MaHDPhanTich { get; set; }
-        public String MaChiTieuPhanTich { get; set; }
-        public uint NamLayMau { get; set; }
-        
-        public virtual Request Request { get; set; }
-
+        //public int MaHDPhanTich { get; set; }
+        public String ChiTieuPhanTich { get; set; }
+        //public uint NamLayMau { get; set; }
     }
 }
