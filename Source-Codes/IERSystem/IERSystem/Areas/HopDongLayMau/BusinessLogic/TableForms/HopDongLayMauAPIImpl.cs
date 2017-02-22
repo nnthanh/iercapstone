2e639fc09bf26d652e08b5206b3c54b5fd74615b
nthoang-apcs <nthoang@apcs.vn>
2017-02-22 19:37:40 +0700

    refers to http://stackoverflow.com/questions/32411148/entity-framework-dbcontext-and-thread-safety. No need to synchronize here.

--- Source-Codes/IERSystem/IERSystem/Areas/HopDongLayMau/BusinessLogic/TableForms/HopDongLayMauAPIImpl.cs
+++ Source-Codes/IERSystem/IERSystem/Areas/HopDongLayMau/BusinessLogic/TableForms/HopDongLayMauAPIImpl.cs
@@ -18,7 +18,6 @@ namespace IERSystem.BusinessLogic.TableForms
     public static partial class HopDongLayMauAPIImpl
     {
         //nthoang: Table Parsing using YeuCauLayMau Input here
-        [MethodImpl(MethodImplOptions.Synchronized)]
         public static void Create(YeuCauLayMauInputModel input_request, IERSystemDBContext db) {
             upsertHopDongLayMau(input_request, db);
             TableForms.CacSoNhanMauAPIImpl.Create(input_request, db);
