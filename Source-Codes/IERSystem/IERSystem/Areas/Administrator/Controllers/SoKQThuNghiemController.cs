f8c4ba5ed290193b14c4e6a488539394625e266b
Thanh Nguyen <Thanh Nguyen>
2017-02-23 10:51:40 +0700

    fix Model of SoKQThuNghiem + Template FormKQ + Template, Model of SoNhanMau

--- Source-Codes/IERSystem/IERSystem/Areas/Administrator/Controllers/SoKQThuNghiemController.cs
+++ Source-Codes/IERSystem/IERSystem/Areas/Administrator/Controllers/SoKQThuNghiemController.cs
@@ -47,7 +47,7 @@ namespace IERSystem.Areas.Administrator.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include="Id,KyHieuMau,NgayNhanMau,NgayTraMau,ChiTieuThuNghiem,DonVi,KetQua,NguoiThucHien")] SoKQThuNghiem sokqthunghiem)
+        public async Task<ActionResult> Create([Bind(Include="Id,KyHieuMau,NgayNhanMau,NgayTraMau")] SoKQThuNghiem sokqthunghiem)
         {
             if (ModelState.IsValid)
             {
@@ -79,7 +79,7 @@ namespace IERSystem.Areas.Administrator.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include="Id,KyHieuMau,NgayNhanMau,NgayTraMau,ChiTieuThuNghiem,DonVi,KetQua,NguoiThucHien")] SoKQThuNghiem sokqthunghiem)
+        public async Task<ActionResult> Edit([Bind(Include="Id,KyHieuMau,NgayNhanMau,NgayTraMau")] SoKQThuNghiem sokqthunghiem)
         {
             if (ModelState.IsValid)
             {
--- Source-Codes/IERSystem/IERSystem/Areas/Administrator/Controllers/SoNhanMauController.cs
+++ Source-Codes/IERSystem/IERSystem/Areas/Administrator/Controllers/SoNhanMauController.cs
@@ -47,7 +47,7 @@ namespace IERSystem.Areas.Administrator.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include="Id,PhieuYeuCau,TenDiaChiKhachHang,MaSo,ChiTieuThuNghiem,NgayNhan,NgayTraKetQua,KHKyNhan,KyHieuMau")] SoNhanMau sonhanmau)
+        public async Task<ActionResult> Create([Bind(Include="Id,PhieuYeuCau,TenDiaChiKhachHang,MaSo,ChiTieuThuNghiem,NgayNhan,NgayTraKetQua,KyHieuMau,KHKyNhanTraTien,KHKyNhanTraKQ")] SoNhanMau sonhanmau)
         {
             if (ModelState.IsValid)
             {
@@ -79,7 +79,7 @@ namespace IERSystem.Areas.Administrator.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include="Id,PhieuYeuCau,TenDiaChiKhachHang,MaSo,ChiTieuThuNghiem,NgayNhan,NgayTraKetQua,KHKyNhan,KyHieuMau")] SoNhanMau sonhanmau)
+        public async Task<ActionResult> Edit([Bind(Include="Id,PhieuYeuCau,TenDiaChiKhachHang,MaSo,ChiTieuThuNghiem,NgayNhan,NgayTraKetQua,KyHieuMau,KHKyNhanTraTien,KHKyNhanTraKQ")] SoNhanMau sonhanmau)
         {
             if (ModelState.IsValid)
             {
