45b07b2d5cf139a7030c9c678d2749dec4cc3126
Ngoc Thanh <nnthanh@apcs.vn>
2017-03-02 12:27:31 +0700

    fix UI version 2

--- Source-Codes/IERSystem/IERSystem/Areas/Administrator/Controllers/CacSoChuyenMauController.cs
+++ Source-Codes/IERSystem/IERSystem/Areas/Administrator/Controllers/CacSoChuyenMauController.cs
@@ -47,7 +47,7 @@ namespace IERSystem.Areas.Administrator.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include="Id,Year,From,To")] CacSoChuyenMau cacsochuyenmau)
+        public async Task<ActionResult> Create([Bind(Include="Id,Year")] CacSoChuyenMau cacsochuyenmau)
         {
             if (ModelState.IsValid)
             {
@@ -79,7 +79,7 @@ namespace IERSystem.Areas.Administrator.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include="Id,Year,From,To")] CacSoChuyenMau cacsochuyenmau)
+        public async Task<ActionResult> Edit([Bind(Include="Id,Year")] CacSoChuyenMau cacsochuyenmau)
         {
             if (ModelState.IsValid)
             {
