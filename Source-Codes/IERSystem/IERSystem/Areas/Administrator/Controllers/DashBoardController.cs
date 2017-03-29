939c2e798f911f7b9ad54f5879200c2d6c9c4e84
ltduy95 <ltduy@apcs.vn>
2017-03-29 19:45:11 +0700

    update controller

--- Source-Codes/IERSystem/IERSystem/Areas/Administrator/Controllers/DashBoardController.cs
+++ Source-Codes/IERSystem/IERSystem/Areas/Administrator/Controllers/DashBoardController.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using IERSystem.Areas.Administrator.Models;
+using System.Data.Objects.SqlClient;
 
 namespace IERSystem.Areas.Administrator.Controllers
 {
@@ -18,8 +19,34 @@ namespace IERSystem.Areas.Administrator.Controllers
         // GET: /Administrator/DashBoard/
         public async Task<ActionResult> Index()
         {
-            var users = db.Users.Include(u => u.RoleMaster);
-            return View(await users.ToListAsync());
+            var danhsachmau = db.MauLayHienTruongs.Include(u=>u.PhieuYeuCau);
+            //Dem tong so hop dong
+            var TongSoHD = db.PhieuYeuCaus.Count();
+            ViewBag.TongSoHD = TongSoHD;
+            //Dem so hop dong vua tao trong vong 3 ngay
+            var HDMoi = (from r in db.PhieuYeuCaus 
+                         where (SqlFunctions.DateDiff("day", DateTime.Now, r.NgayTaoHD) < 3) 
+                         select r).Count();
+            ViewBag.HDMoi = HDMoi;
+            //Dem so mau da duoc nhan
+            var SoMauDaNhan = (from m in db.MauLayHienTruongs where(m.TinhTrang==1)select m).Count();
+            ViewBag.SoMauDaNHan = SoMauDaNhan;
+            //Dem so mau da nhan trong vong 1 tuan
+            var SoMauMoiNhan = (from m in db.MauLayHienTruongs
+                                join so in db.SoNhanMaus on m.Id equals so.MauLayHienTruong.Id
+                                where (m.TinhTrang == 1 && (SqlFunctions.DateDiff("day", DateTime.Now, m.SoNhanMau.NgayNhanMau) < 7)) 
+                                select m).Count();
+            ViewBag.SoMauMoiNhan = SoMauMoiNhan;
+            //Dem so mau da duoc thi nghiem
+            var SoMauDaDuocTN = (from m in db.MauLayHienTruongs where(m.TinhTrang==3)select m).Count();
+            ViewBag.SoMauDaDuocTN = SoMauDaDuocTN;
+            //Dem so mau da duoc thi nghiem trong vong 1 tuan
+            var SoMauMoiTN = (from m in db.MauLayHienTruongs
+                              join so in db.SoKQThuNghiems on m.Id equals so.MauLayHienTruong.Id
+                              where (m.TinhTrang == 3 && (DateTime.Now - (DateTime)m.SoKQThuNghiem.NgayNhanMau).TotalDays < 7)
+                              select m).Count();
+            ViewBag.SoMauMoiTN = SoMauMoiTN;
+            return View(await danhsachmau.ToListAsync());
         }
 
         protected override void Dispose(bool disposing)
