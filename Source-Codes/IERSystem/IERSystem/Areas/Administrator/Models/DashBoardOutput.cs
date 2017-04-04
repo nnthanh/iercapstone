8bb09e6a2e82a19eea3fc0dd4d8711530b8f3af6
ltduy95 <ltduy@apcs.vn>
2017-04-04 21:16:22 +0700

    update

---
+++ Source-Codes/IERSystem/IERSystem/Areas/Administrator/Models/DashBoardOutput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IERSystem.Areas.Administrator.Models
+{
+    public class DashBoardOutputModel
+    {
+        public IEnumerable<MauLayHienTruong> DanhSachMau { get; set; }
+        public int TongSoHD { get; set; }
+        public int HopDongMoi { get; set; }
+        public int SoMauDaNhan { get; set; }
+        public int SoMauMoiNhan { get; set; }
+        public int SoMauDaTN { get; set; }
+        public int SoMauMoiTN { get; set; }
+
+    }
+}
