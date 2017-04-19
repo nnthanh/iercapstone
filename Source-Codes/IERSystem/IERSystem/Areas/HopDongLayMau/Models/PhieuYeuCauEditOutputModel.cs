627eaa641956ac666beb1882a50999e9125111b7
nthoang-apcs <nthoang@apcs.vn>
2017-04-19 20:43:03 +0700

    Initial work

---
+++ Source-Codes/IERSystem/IERSystem/Areas/HopDongLayMau/Models/PhieuYeuCauEditOutputModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IERSystem.Areas.HopDongLayMau.Models
+{
+    public class PhieuYeuCauEditOutputModel
+    {
+        public string TenKhachHang { get; set; }
+        public string MaSoThue { get; set; }
+        public string TenDaiDien { get; set; }
+        public string SoDienThoai { get; set; }
+        public string SoFax { get; set; }
+        public string DiaChiLayMau { get; set; }
+        public string DiaChiKhachHang { get; set; }
+        public System.DateTime NgayLayMau { get; set; }
+        public System.DateTime NgayHenTraKQ { get; set; }
+
+        public virtual ICollection<MauLayHienTruongEditOutputModel> MauLayHienTruongs { get; set; }
+    }
+
+    public class MauLayHienTruongEditOutputModel
+    {
+        public string MaMau { get; set; }
+        public string MaMauKH { get; set; }
+        public string ViTriLayMau { get; set; }
+        public int SoLuong { get; set; }
+        public string MoTaMau { get; set; }
+        public string ChiTieuPTs { get; set; }
+    }
+}
